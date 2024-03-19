1253c73b892f40de9274a8fd235b3c3f91ad1b29
roman-cossackov <roman.delarge@gmail.com>
2024-03-20 02:54:55 +0300

    fixes

--- src/components/CurrencySwitcher.tsx
+++ src/components/CurrencySwitcher.tsx
@@ -1,6 +1,6 @@
 import { Box, Button } from '@mui/material';
 
-import ExchangeIcon from '../ExchangeIcon/ExchangeIcon';
+import ExchangeIcon from '../icons/ExchangeIcon/ExchangeIcon';
 
 interface CurrencySwitcherProps {
   onSwitchCurrencies: (event: React.MouseEvent<HTMLElement>) => void;
